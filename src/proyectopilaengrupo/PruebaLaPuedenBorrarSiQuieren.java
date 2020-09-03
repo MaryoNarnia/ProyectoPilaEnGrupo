0459853ece8bf6cb847ac1ba641b127d5fd48a2c
MaryoNarnia <70710834+MaryoNarnia@users.noreply.github.com>
2020-09-03 10:50:01 -0500

    Create PruebaLaPuedenBorrarSiQuieren.java
    holi solo escribí algo

---
+++ src/proyectopilaengrupo/PruebaLaPuedenBorrarSiQuieren.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopilaengrupo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PruebaLaPuedenBorrarSiQuieren {
+    private int jdfgjdfkgjdkjgkdf;
+    
+}
